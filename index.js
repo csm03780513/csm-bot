ba1b38ccad048ec44e416af19efdbd096912dc76
rmurray <rmurray@safaricom.co.ke>
2019-08-23 10:17:17 +0300

    nasdjs

--- index.js
+++ index.js
@@ -125,8 +125,20 @@ bot.on('message', (msg) => {
     if (msg.text.toLowerCase().includes('hi', 'hey')) {
         // send a message to the chat acknowledging receipt of their message
         return bot.sendMessage(chatId, 'Hello ' + msg.chat.first_name + ' ' + msg.chat.last_name + '\u{1F603}' + '\u{1F609}');
+    } else if (msg.text.toLowerCase.includes('browser')) {
+return bot.sendMessage(
+    chatId,
+    'Play Game Using Broser',
+    reply_markup, {
+        inline_keyboard: [[{
+            text: 'Share with your friends',
+            switch_inline_query: 'share'
+        }]]
+    }
+)
     } else {
         return bot.sendMessage(chatId, 'Mmh, trying to be smart with me? \u{1F60F} I am still learning, I offer only games here. say something like->I want to play a game or click-> /play');
+
     }
 });
 
