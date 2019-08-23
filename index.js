2d89d9ef6b1f168c1fd54ccf2f0d09560851fd39
rmurray <rmurray@safaricom.co.ke>
2019-08-23 10:48:09 +0300

    chatid

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ bot.on('message', (msg) => {
         // send a message to the chat acknowledging receipt of their message
         return bot.sendMessage(chatId, 'Hello ' + msg.chat.first_name + ' ' + msg.chat.last_name + '\u{1F603}' + '\u{1F609}');
     } else if (msg.text.toLowerCase().includes('browser')) {
-        return bot.sendMessage(chat_id, 'HELLO MNBH');
+        return bot.sendMessage(chatId, 'HELLO MNBH');
     } else {
         return bot.sendMessage(chatId, 'Mmh, trying to be smart with me? \u{1F60F} I am still learning, I offer only games here. say something like->I want to play a game or click-> /play');
     }
